fb8ba152bfe39615372ef3c2be432f4b56a2f74c
stephanfdo <k.stephanfdo@gmail.com>
2022-02-28 23:17:06 +0530

    update user page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,54 +6,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Stellar Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
-    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="css1/simple-line-icons.css">
+    <link rel="stylesheet" href="css1/flag-icon.min.css">
+    <link rel="stylesheet" href="css1/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="./vendors/daterangepicker/daterangepicker.css">
-    <link rel="stylesheet" href="./vendors/chartist/chartist.min.css">
+    <link rel="stylesheet" href="./css1/daterangepicker.css">
+    <link rel="stylesheet" href="./css1/chartist.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css1/userstyle.css">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="./images/favicon.png" />
   </head>
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_navbar.html -->
-      <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <nav style="background-color: black ;" class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="navbar-brand-wrapper d-flex align-items-center">
           <a class="navbar-brand brand-logo" href="index.html">
-            <img src="images/MYMEAL.png" alt="logo" class="logo-dark" />
+
           </a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img style="width: 200px; height: 200px;" src="images/MYMEAL.png" alt="logo" /></a>
+
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center flex-grow-1">
-
+         <img style="height:70px;" src="images/MYMEAL.png" alt="logo" class="logo-dark" />
           <ul class="navbar-nav navbar-nav-right ml-auto">
+      <li></li>
+
             <form class="search-form d-none d-md-block" action="#">
               <i class="icon-magnifier"></i>
               <input type="search" class="form-control" placeholder="Search Here" title="Search here">
             </form>
 
 
-            <li class="nav-item dropdown">
-              <a class="nav-link count-indicator message-dropdown" id="messageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <i class="icon-speech"></i>
-                <span class="count">7</span>
-              </a>
+
 
 
             <li class="nav-item dropdown d-none d-xl-inline-flex user-dropdown">
               <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <img class="img-xs rounded-circle ml-2" src="images/faces/face8.jpg" alt="Profile image"> <span class="font-weight-normal"> Henry Klein </span></a>
+                <img class="img-xs rounded-circle ml-2" src="images/faces/.jpg" alt="Profile image"> <span class="font-weight-normal">  </span></a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                 <div class="dropdown-header text-center">
                   <img class="img-md rounded-circle" src="images/faces/face8.jpg" alt="Profile image">
-                  <p class="mb-1 mt-3">Allen Moreno</p>
+                  <p class="mb-1 mt-3"></p>
                   <p class="font-weight-light text-muted mb-0">allenmoreno@gmail.com</p>
                 </div>
                 <a class="dropdown-item"><i class="dropdown-item-icon icon-user text-primary"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
@@ -90,56 +88,30 @@
                 </div>
               </a>
             </li>
-            <li class="nav-item nav-category">
-              <span class="nav-link">Dashboard</span>
-            </li>
+
             <li class="nav-item">
               <a class="nav-link" href="index.html">
-                <span class="menu-title">Dashboard</span>
-                <i class="icon-screen-desktop menu-icon"></i>
+                <div class="nav-item nav-category">
+              <span class="nav-link">Dashboard</span>
+            </div>
               </a>
+
             </li>
-            <li class="nav-item nav-category"><span class="nav-link">UI Elements</span></li>
+
+
             <li class="nav-item">
               <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <span class="menu-title">Basic UI Elements</span>
+                <div class="nav-item nav-category"><span class="nav-link">UI Elements</span></div>
                 <i class="icon-layers menu-icon"></i>
               </a>
-              <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                  <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-                </ul>
-              </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/icons/simple-line-icons.html">
-                <span class="menu-title">Icons</span>
-                <i class="icon-globe menu-icon"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/forms/basic_elements.html">
-                <span class="menu-title">Form Elements</span>
-                <i class="icon-book-open menu-icon"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/charts/chartist.html">
-                <span class="menu-title">Charts</span>
-                <i class="icon-chart menu-icon"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/tables/basic-table.html">
-                <span class="menu-title">Tables</span>
-                <i class="icon-grid menu-icon"></i>
-              </a>
-            </li>
-            <li class="nav-item nav-category"><span class="nav-link">Sample Pages</span></li>
+
+
+
+
             <li class="nav-item">
               <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <span class="menu-title">General Pages</span>
+                <div class="nav-item nav-category"><span class="nav-link">Sample Pages</span></div>
                 <i class="icon-doc menu-icon"></i>
               </a>
               <div class="collapse" id="auth">
@@ -162,20 +134,20 @@
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <script src="js1/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="./vendors/chart.js/Chart.min.js"></script>
-    <script src="./vendors/moment/moment.min.js"></script>
-    <script src="./vendors/daterangepicker/daterangepicker.js"></script>
-    <script src="./vendors/chartist/chartist.min.js"></script>
+    <script src="./js1/Chart.min.js"></script>
+    <script src="./js1/moment.min.js"></script>
+    <script src="./js1/daterangepicker.js"></script>
+    <script src="./js1/chartist.min.js"></script>
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/misc.js"></script>
+    <script src="js1/off-canvas.js"></script>
+    <script src="js1/misc.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page -->
-    <script src="./js/dashboard.js"></script>
+    <script src="./js1/dashboard.js"></script>
     <!-- End custom js for this page -->
   </body>
 </html>
